1c3d61414ebe3c7a2d182abefe194c94687b8fed
Alexander Esteban Zapata <AlexanderEstebanZapata1994@users.noreply.github.com>
2020-06-06 15:01:54 -0500

    init commit

---
+++ build/precache-manifest.82636b7cfce52a028ec4790f805a606a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daef0ca6a5d258f0b57346c0d46e36b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff366c52c452cf8950b1",
+    "url": "/static/css/main.9aad2567.chunk.css"
+  },
+  {
+    "revision": "09d22a5fad388e5a7561",
+    "url": "/static/js/2.d021bcca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d021bcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff366c52c452cf8950b1",
+    "url": "/static/js/main.b6be85b4.chunk.js"
+  },
+  {
+    "revision": "c057eb8eb957bb3d68b8",
+    "url": "/static/js/runtime-main.9a78f4ec.js"
+  }
+]);
